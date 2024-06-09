37db141ee79fbcad16cd82122f8a5f2df3955912
rfspringer <rachel@gardner-springer.com>
2024-06-09 13:06:55 -0500

    commented + refactored simulations

--- simulations.py
+++ simulations.py
@@ -2,7 +2,7 @@ import networkx as nx
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
-from network_sort import NetworkSort
+import network_sort
 import utils
 from scipy.stats import spearmanr
 import pandas as pd
@@ -17,20 +17,37 @@ class Simulation:
         wealth_to_income_fn=lambda x: x * 0.2,
         **wealth_kwargs
     ):
+        """
+        Initializes a new graph simulation
+        :param num_nodes: Number of nodes for simulated graph
+        :param prob_connection: Probability of each pair of nodes being connected in simulation
+        :param wealth_dist: Distribution function for wealth, ex. np.random.normal, np.random.randint. By default uses randint from 0 - 1000000
+        :param wealth_to_income_fn: Function for calculating income from wealth. By default 0.2 * wealth
+        :param wealth_kwargs: Args for provided wealth distribution function
+        """
         self.id_to_wealth = self.make_id_to_wealth_dict(
             num_nodes, wealth_dist, **wealth_kwargs
         )
         self.id_to_income = self.make_id_to_income_dict(wealth_to_income_fn)
         self.true_sorted_ids, self.ranks_by_id = self.sort_ids_by_wealth()
         self.G = self.make_graph(num_nodes, prob_connection)
-        self.sorted_nodes = NetworkSort(
+        self.sorted_nodes = network_sort.sort(
             self.G
-        ).sort()  # note this comes out at list of sets (in case of ties), not list of elements
+        )  # note this comes out at list of sets (in case of ties), not list of elements
         self.rank_correlation, self.best_rank_correlation_list = (
             self.calculate_rank_correlation()
         )
 
-    def make_id_to_wealth_dict(self, num_nodes, wealth_dist, **kwargs):
+    def make_id_to_wealth_dict(self, num_nodes, wealth_dist, **kwargs) -> {int: float}:
+        """
+        Make dictionary of node ids to wealth, generated from wealth_dist function
+        :param num_nodes: Number of nodes in the simulation
+        :param wealth_dist: Distribution to use for generating wealth. By default np.random.randint from 0 - 1000000
+        :param kwargs: Args for wealth_dist function.
+        ex {"low": ____, "high": ____} for randint, {"loc": ___, "scale": ___} for normal
+        :return: Dictionary of node ids to wealth
+        """
+
         # Set default distribution function to np.random.randint with low=0 and high=100000
         if wealth_dist is None:
             wealth_dist = np.random.randint
@@ -40,19 +57,34 @@ class Simulation:
         wealths = wealth_dist(size=num_nodes, **kwargs)
         return {i: wealths[i] for i in range(num_nodes)}
 
-    def make_id_to_income_dict(self, wealth_to_income_fn):
+    def make_id_to_income_dict(self, wealth_to_income_fn) -> {int: float}:
+        """
+        Create id to income dictionary based from wealth-to-income function
+        :param wealth_to_income_fn: function for converting wealth to income
+        :return: Dictionary of node ids to income
+        """
         return {
             id: wealth_to_income_fn(wealth) for id, wealth in self.id_to_wealth.items()
         }
 
-    def sort_ids_by_wealth(self):
+    def sort_ids_by_wealth(self) -> ([int], {int: int}):
+        """
+        Sort simulated nodes by wealth
+        :return: list of ids sorted by wealth, dictionary of node ids to rank
+        """
         sorted_ids = sorted(
             self.id_to_wealth.keys(), key=lambda k: self.id_to_wealth[k], reverse=True
         )
         ranks = {id_: rank for rank, id_ in enumerate(sorted_ids, start=1)}
         return sorted_ids, ranks
 
-    def make_graph(self, num_nodes, prob_connection=0.5):
+    def make_graph(self, num_nodes: int, prob_connection: float) -> nx.DiGraph:
+        """
+        Build graph from graph parameters and incomes
+        :param num_nodes: Number of nodes for graph to have
+        :param prob_connection: Probability of an edge between any two nodes
+        :return: Simulated weighted directed graph of nodes, with weights/ direction of income difference
+        """
         # create list of all pairs of ids
         pairs = list(itertools.combinations(self.id_to_income.keys(), 2))
 
@@ -69,17 +101,28 @@ class Simulation:
                 self.add_edge(G, pair)
         return G
 
-    def add_edge(self, G, pair):
-        a, b = pair[0], pair[1]
+    def add_edge(self, G: nx.DiGraph, node_pair: (int, int)):
+        """
+        Add an edge between two nodes, pointing from higher income node to lower income node, with weight of difference in incomes
+        :param G: Graph to add edge to
+        :param node_pair: tuple of two node ids to connect
+        :return:
+        """
+        a, b = node_pair[0], node_pair[1]
         weight = self.id_to_income[a] - self.id_to_income[b]
 
-        # add edge with weight of income difference, pointing in direciton from higher to lower income
+        # add edge with weight of income difference, pointing in direction from higher to lower income
+        # if incomes equal, direction is effectively random
         if weight > 0:
             G.add_edge(a, b, weight=weight)
         else:
             G.add_edge(b, a, weight=-weight)
 
     def show(self):
+        """
+        Display a visualization of the simulated graph
+        :return:
+        """
         positions = nx.circular_layout(self.G)  # Compute node positions
 
         # Draw nodes and edges
@@ -99,11 +142,18 @@ class Simulation:
 
         plt.show()
 
-    def calculate_rank_correlation(self):
+    def calculate_rank_correlation(self) -> (float, [int]):
+        """
+        Calculate spearman rank correlation between true sorted order of wealth and results from sorting algorithm
+        :return: spearman rank correlation between true order and sorting algorithm results, list used to calculate result
+        """
+
+        # all possible orderings of elements from the sorting algorithm (in case of ties, though this is infrequent unless weights are often the same)
         all_lists = utils.get_list_of_set_permutations(self.sorted_nodes)
+
+        # checks through all possible orders within sets to get closest to correct one (since all would be valid orderings)
         best_list = None
         max_rank_correl = -1
-
         for list in all_lists:
             list_ranks = [self.ranks_by_id[node] for node in list]
             true_ranks = [self.ranks_by_id[node] for node in self.true_sorted_ids]
